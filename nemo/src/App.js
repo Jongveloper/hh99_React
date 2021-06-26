62e49c3834510b6db84ce93672ea8b7c34fb4790
Jongveloper <59111836+Jongveloper@users.noreply.github.com>
2021-06-26 20:19:10 +0900

    리액트 공부

---
+++ nemo/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Nemo from "./Nemo";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 3,
+    };
+  }
+
+  addNemo = () => {
+    this.setState({count: this.state.count + 1});
+    console.log('add nemo');
+  }
+
+  removeNemo = () => {
+    if(this.state.count > 0){
+      this.setState({count: this.state.count -1});
+    }else{
+      window.alert('네모가 없어요!')
+    }
+    
+    console.log('remove nemo');
+  }
+
+  render() {
+    const nemo_count = Array.from({ length: this.state.count }, (v, i) => (i))
+    console.log(nemo_count);
+    return (
+      <div className="App">
+        <Nemo/>
+      </div>
+
+    )
+  }
+}
+export default App;
